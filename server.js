b838faf01dfc5eacf4995d097940456517f4e42f
Your Name <harrishtet.nyanlinhtet@gmail.com>
2025-05-02 13:28:16 -0700

    fully setted up mongodb and retreival and commands

--- server.js
+++ server.js
@@ -2,6 +2,37 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const port = 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://nyanlinhtet:D3menter!@custermain.iddklp5.mongodb.net/?retryWrites=true&w=majority&appName=Custermain";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+}});
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      const db = client.db("sample_mflix");            // choose (or create) your database
+      const users = db.collection("comments");           // choose (or create) your collection
+      const allUsers = await users.find({}).toArray();
+      console.log("All users:", allUsers);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+}
+
+
+
+run().catch(console.dir);
 
 app.use('/html', express.static(path.join(__dirname, 'html')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
@@ -16,3 +47,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
